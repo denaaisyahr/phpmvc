1832fa4623c05559a8d625355d70f15ca4b3bc8a
denaaisyahr <denaaisyah14@gmail.com>
2019-10-07 15:01:29 +0700

    menyelesaikan sampai video ke 8 dan sudah sampai database wrapper

---
+++ app/views/about/index.php
@@ -0,0 +1,5 @@
+
+	<h1 class="mt-4">About Me</h1>
+	<img src="<?= BASEURL; ?>/img/a.jpg" alt="Dena Aisyah Rachman" width="200" class="rounded-circle shadow">
+	<p>Hallo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+</div>
